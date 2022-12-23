3ea7d9ce77d32163c8b33962ff10047b65f9baa1
pranabpaul-tech <pranabp@microsoft.com>
2022-12-23 13:49:06 +0000

    Revert "changed DB structure"
    This reverts commit fce534c532becab2dc1bced673907b8604edb626.

---
+++ api/API/LevelAPI/Controllers/LevelsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LevelAPI.DBService;
+using LevelAPI.Models;
+
+namespace LevelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LevelsController : ControllerBase
+    {
+        private readonly mydbContext _context;
+
+        public LevelsController(mydbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Levels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Level>>> GetLevels()
+        {
+            return await _context.Levels.ToListAsync();
+        }
+
+        // GET: api/Levels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Level>> GetLevel(int id)
+        {
+            var level = await _context.Levels.FindAsync(id);
+
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            return level;
+        }
+
+        // PUT: api/Levels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLevel(int id, Level level)
+        {
+            if (id != level.Levelid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(level).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LevelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Levels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Level>> PostLevel(Level level)
+        {
+            _context.Levels.Add(level);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLevel", new { id = level.Levelid }, level);
+        }
+
+        // DELETE: api/Levels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLevel(int id)
+        {
+            var level = await _context.Levels.FindAsync(id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            _context.Levels.Remove(level);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LevelExists(int id)
+        {
+            return _context.Levels.Any(e => e.Levelid == id);
+        }
+    }
+}
---
+++ api/API/ResourcedetailsAPI/Models/Resourcedetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+namespace ResourcedetailsAPI.Models
+{
+    public partial class Resourcedetail
+    {
+        public int Resourcedetailid { get; set; }
+        public string Projectname { get; set; }
+        public string Resourcegroupname { get; set; }
+        public string Subscriptionid { get; set; }
+        public string Projectowneremail { get; set; }
+        public int Levelid { get; set; }
+    }
+}
