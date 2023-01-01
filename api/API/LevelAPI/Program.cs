7886c9ff4d1dacc8ef31489aa71733274488085d
pranabpaul-tech <pranabp@microsoft.com>
2023-01-01 11:04:29 +0000

    added swagger in production

--- api/API/LevelAPI/Program.cs
+++ api/API/LevelAPI/Program.cs
@@ -48,11 +48,11 @@ internal class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        //if (app.Environment.IsDevelopment())
+        //{
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        //}
 
         app.UseHttpsRedirection();
 
