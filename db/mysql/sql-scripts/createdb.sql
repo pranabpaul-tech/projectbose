2ca21a61e2c7b78d81da257f08a1e76079a7f8ae
pranabpaul-tech <pranabp@microsoft.com>
2023-01-14 17:29:56 +0000

    levelinfo added

--- db/mysql/sql-scripts/createdb.sql
+++ db/mysql/sql-scripts/createdb.sql
@@ -13,7 +13,6 @@ create table leveldetail(
    leveldetailname VARCHAR(100) NOT NULL,
    levelid INT NOT NULL,
    superleveldetailid INT NOT NULL,
-   squenceid INT NOT NULL,
    PRIMARY KEY ( leveldetailid )
 );
 
@@ -27,20 +26,80 @@ create table resourcedetail(
    PRIMARY KEY ( resourcedetailid )
 );
 
--- CREATE PROCEDURE leveldetailpath_sp 
--- BEGIN
--- WITH RECURSIVE leveldetail_path (leveldetailid, leveldetailname, levelid, path) AS
--- (
---   SELECT leveldetailid, leveldetailname, levelid, leveldetailname as path
---     FROM leveldetail
---     WHERE superleveldetailid = 0
---   UNION ALL
---   SELECT c.leveldetailid, c.leveldetailname, c.levelid, CONCAT(cp.path, ' > ', c.leveldetailname)
---     FROM leveldetail_path AS cp JOIN leveldetail AS c
---       ON cp.leveldetailid = c.superleveldetailid
--- )
--- select leveldetailid, leveldetailname, path from leveldetail_path where levelid = (select MAX(levelid) from level) ;
--- END;
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS CreateLevelInfo$$
+CREATE PROCEDURE CreateLevelInfo(
+      --   IN id_array VARCHAR(1000), OUT outputsql VARCHAR(1000)
+      IN id_array VARCHAR(1000)
+    )
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE id_array_local VARCHAR(1000);
+    DECLARE start_pos SMALLINT;
+    DECLARE comma_pos SMALLINT;
+    DECLARE current_id VARCHAR(1000);
+    DECLARE end_loop TINYINT;
+
+   --  DECLARE @finalsql VARCHAR(1000);
+    DECLARE startofsql VARCHAR(100);
+    DECLARE midofsql VARCHAR(100);
+    DECLARE endofsql VARCHAR(100);
+    DECLARE selectofsql VARCHAR(1000);
+    DECLARE joinofsql VARCHAR(1000);
+    DECLARE loopindex SMALLINT;
+    DECLARE loopindex2 SMALLINT;
+    
+    SET @finalsql = '';
+    SET startofsql = 'create table levelinfo as select ';
+    SET midofsql = ' FROM leveldetail AS t1 ';
+    SET endofsql = ' WHERE t1.levelid = 1';
+    SET selectofsql = '';
+    SET joinofsql = '';
+    SET loopindex = 1;
+    SET loopindex2 = 0;
+
+    SET id_array_local = id_array;
+    SET start_pos = 1;
+    SET comma_pos = locate(',', id_array_local);
+    REPEAT
+        IF comma_pos > 0 THEN
+            SET current_id = substring(id_array_local, start_pos, comma_pos - start_pos);
+            SET end_loop = 0;
+        ELSE
+            SET current_id = substring(id_array_local, start_pos);
+            SET end_loop = 1;
+        END IF;
+        
+        SET selectofsql = CONCAT(selectofsql,' t',loopindex,'.leveldetailname AS ',current_id,',');
+        IF loopindex > 1 THEN
+            SET loopindex2 = loopindex-1;
+        	SET joinofsql = CONCAT(joinofsql,' LEFT JOIN leveldetail AS t',loopindex,' ON t',loopindex,'.superleveldetailid = t',loopindex2,'.leveldetailid ');
+        END IF;     
+        # Place here your code that uses current_id
+       
+        IF end_loop = 0 THEN
+            SET id_array_local = substring(id_array_local, comma_pos + 1);
+            SET comma_pos = locate(',', id_array_local);
+        END IF;
+        SET loopindex = loopindex + 1;
+    UNTIL end_loop = 1
+    END REPEAT;
+    IF end_loop = 1 THEN
+       SET loopindex = loopindex - 1;
+    END IF;
+    SET selectofsql = CONCAT(selectofsql,' t',loopindex,'.leveldetailid');
+    SET @finalsql = CONCAT(startofsql,selectofsql,midofsql,joinofsql,endofsql); 
+   --  SET outputsql = @finalsql;
+   --  select @outputsql;
+   PREPARE stmt FROM @finalsql;
+   EXECUTE stmt;
+   DEALLOCATE PREPARE stmt;
+END$$
+
+DELIMITER ;
+
 
 CREATE USER 'aksuser'@'10.0.0.%' IDENTIFIED BY 'password123';
 GRANT ALL PRIVILEGES ON mydb.* TO 'aksuser'@'10.0.0.%';
