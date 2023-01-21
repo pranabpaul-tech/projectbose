49cf6c1ee91da6887648746337539ba40e8055ff
pranabpaul-tech <pranabp@microsoft.com>
2023-01-21 22:41:13 +0000

    changed cron

--- db/cassandra/insertdata.sh
+++ db/cassandra/insertdata.sh
@@ -1,2 +1,4 @@
-cqlsh -u aksuser -p password123 --execute=copy azuredata.azurecost from '/mnt/blob/output/output.csv' with header = true
+cqlsh cassandraset-0 9042 -u aksuser -p password123
+copy azuredata.azurecost (resourceid, resourcename, resourcetype, resourcegroupname, subscriptionid, region, usagedate, project, projectowner, cost, bu, division, costcenter) from '/mnt/blob/output/output.csv' with header = true;
+exit
 rm -rf /mnt/blob/output/output.csv || true
