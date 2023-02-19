13816ff2844c55c1b7bd4913dcf58cd604c6c28c
pranabpaul-tech <pranabp@microsoft.com>
2023-02-19 12:01:26 +0000

    changed name to output data

--- db/cassandra/insertdata.py
+++ db/cassandra/insertdata.py
@@ -5,8 +5,8 @@ import os
 
 # Command to execute
 # Using Windows OS command
-command = f"""cqlsh localhost -u cassandra -p 'cassandra' -k 'azuredata' -e \" COPY azuredata.azurecost FROM '/mnt/blob/output/output.csv' with header = true; \" """
+command = f"""cqlsh localhost -u cassandra -p 'cassandra' -k 'azuredata' -e \" COPY azuredata.azurecost FROM '/mnt/blob/output/outputdata.csv' with header = true; \" """
 
 # Using os.system() method
 os.system(command)
-os.remove("/mnt/blob/output/output.csv")
+os.remove("/mnt/blob/output/outputdata.csv")
