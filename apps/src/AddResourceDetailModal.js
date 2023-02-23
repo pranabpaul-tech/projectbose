fb6b52480243afc29fa3960a8c446d4d16dcceee
pranabpaul-tech <pranabp@microsoft.com>
2023-02-23 18:45:59 +0000

    first commit

---
+++ apps/src/AddResourceDetailModal.js
@@ -0,0 +1,100 @@
+import React,{Component} from 'react';
+import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+
+export class AddResourceDetailModal extends Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    
+    handleSubmit(event){
+        event.preventDefault();
+        const requestOptions = {
+            method: 'POST',
+            body: JSON.stringify({resourcedetailid: event.target.ResourceDetailId.value,
+                projectname: event.target.ProjectName.value,
+                resourcegroupname: event.target.ResourceGroupName.value,
+                subscriptionid: event.target.SubscriptionId.value,
+                projectowneremail: event.target.ProjectOwnerEmail.value,
+                leveldetailid: event.target.LevelDetailId.value})
+        };
+        fetch(process.env.REACT_APP_API+'Resourcedetails', requestOptions)
+            .then(response => response.json())
+            .then((result)=>{
+                alert(JSON.stringify(result));
+            },
+            (error)=>{
+                alert('Failed');
+            })
+    }        
+    render(){
+        return (
+            <div className="container">
+
+<Modal
+{...this.props}
+size="lg"
+aria-labelledby="contained-modal-title-vcenter"
+centered
+>
+    <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+            Add ResourceDetail
+        </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+
+        <Row>
+            <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="ResourceDetailId">
+                        <Form.Label>ResourceDetailId</Form.Label>
+                        <Form.Control type="text" name="ResourceDetailId" required 
+                        placeholder="ResourceDetailId"/>
+                    </Form.Group>
+                    <Form.Group controlId="ProjectName">
+                        <Form.Label>ProjectName</Form.Label>
+                        <Form.Control type="text" name="ProjectName" required 
+                        placeholder="ProjectName"/>
+                    </Form.Group>
+                    <Form.Group controlId="ResourceGroupName">
+                        <Form.Label>ResourceGroupName</Form.Label>
+                        <Form.Control type="text" name="ResourceGroupName" required 
+                        placeholder="ResourceGroupName"/>
+                    </Form.Group>
+                    <Form.Group controlId="SubscriptionId">
+                        <Form.Label>SubscriptionId</Form.Label>
+                        <Form.Control type="text" name="SubscriptionId" required 
+                        placeholder="SubscriptionId"/>
+                    </Form.Group>
+                    <Form.Group controlId="ProjectOwnerEmail">
+                        <Form.Label>ProjectOwnerEmail</Form.Label>
+                        <Form.Control type="text" name="ProjectOwnerEmail" required 
+                        placeholder="ProjectOwnerEmail"/>
+                    </Form.Group>
+                    <Form.Group controlId="LevelDetailId">
+                        <Form.Label>LevelDetailId</Form.Label>
+                        <Form.Control type="text" name="LevelDetailId" required 
+                        placeholder="LevelDetailId"/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Button variant="primary" type="submit">
+                            Add Resource
+                        </Button>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
+    </Modal.Body>
+    
+    <Modal.Footer>
+        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+    </Modal.Footer>
+
+</Modal>
+
+            </div>
+        )
+    }
+
+}
