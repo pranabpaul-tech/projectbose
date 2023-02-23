40c32d6365bb443b4f20ec7638b301f9de6cffa1
pranabpaul-tech <pranabp@microsoft.com>
2023-02-23 23:49:13 +0000

    small changes

--- api/API/ResourcedetailsAPI/Program.cs
+++ api/API/ResourcedetailsAPI/Program.cs
@@ -25,7 +25,6 @@ internal class Program
                               });
         });
         // Add services to the container.
-
         ConfigurationManager configuration = builder.Configuration;
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
